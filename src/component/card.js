0a361483b24503c35d4ef6efb286b72192ccda7d
Victor Tran <victor.n.tran@outlook.com>
2019-11-01 20:06:18 -0700

    add children prop

--- src/component/card.js
+++ src/component/card.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Card = ({className = 'Card'}) => {
+const Card = ({className = 'Card', children}) => {
   return (
     <section className={className}>
-      
+      {children}
     </section>
   )
 }
