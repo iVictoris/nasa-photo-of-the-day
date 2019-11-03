ab6b768fe638a90fa909a6aea514296c47cb91b7
Victor Tran <victor.n.tran@outlook.com>
2019-11-02 18:44:33 -0700

    account for when media_type isn't an image, send different request with specified date attached

--- src/component/PhotoCard.js
+++ src/component/PhotoCard.js
@@ -1,46 +1,62 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import Card from './Card';
-import Caption from './Caption';
-import Image from './Image';
-import Title from './Title';
+import Card from "./Card";
+import Caption from "./Caption";
+import Image from "./Image";
+import Title from "./Title";
 
-const PhotoCard = ({className = 'PhotoCard', apiKey = 'DEMO_KEY'}) => {
+const PhotoCard = ({ className = "PhotoCard", apiKey = "DEMO_KEY" }) => {
+  const extractData = data => {
+    if (data) {
+      const { explanation, hdurl, url, title, media_type } = data;
+      const neededData = {
+        explanation,
+        hdurl,
+        url,
+        title,
+        media_type
+      };
+      return neededData;
+    }
+  };
   const [data, setData] = useState({});
 
   useEffect(() => {
     // fetch data to...
     // https://api.nasa.gov/planetary/apod?api_key=${apiKey}
+    const apiRequest = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
 
     axios
-    .get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`).then(({data}) => {
-      const {explanation, hdurl, url, title, media_type} = data;
-      const neededData = {
-        explanation, hdurl, url, title, media_type
-      }
-      setData(neededData)
-    })
-    .catch(console.log);
-    // data = {
-      // explanation,
-      // hdurl,
-      // url,
-      // title,
-      // media_type
-    }, []);
+      .get(apiRequest)
+      .then(({ data }) => {
+        const { media_type, ...neededData } = extractData(data);
+        if (media_type !== "image") {
+          return axios.get(apiRequest.concat(`&date=2012-03-14`));
+        }
+        setData(neededData);
+      })
+      .then((newData) => {
+        if (newData) {
+          const {data} = newData;
+          const neededData = extractData(data);
+          setData(neededData);
+        }
+      })
+      .catch(console.log);
+  }, []);
 
-    const {title, url, explanation} = data;
-  
+  const { title, url, explanation } = data;
+
   return (
     <section className={className}>
       <Card>
-        <Title value={title}/>
-        <Image src={url}/>
-        <Caption value={explanation}/>
+        <Title value={title} />
+        <Image src={url} />
+        <Caption value={explanation} />
       </Card>
     </section>
-  )
-}
+  );
+};
 
-export default PhotoCard
+export default PhotoCard;
